3d4fe3e83d4de2b7940a509991b5c05ee9066291
rongxiaoqu <rongxiaoqu@gmail.com>
2023-04-21 05:10:39 +0800

    Update train_rvs.py

--- SMARTS-comp-1/competition/fanta-code-main/train_rvs.py
+++ SMARTS-comp-1/competition/fanta-code-main/train_rvs.py
@@ -149,7 +149,7 @@ def train(input_path, output_suffix, conservative_weight):
 
     # filename = scenario_path / (f"Agent-history-vehicle-{1}.pkl")
 
-    filename = "Agent-history-vehicle-1.pkl"
+    filename = "offline-datasets-master/us101_candid_list.pkl"
     # print(scenario_path)
     with open(filename, 'rb') as f:
         contents = pickle.load(f)
