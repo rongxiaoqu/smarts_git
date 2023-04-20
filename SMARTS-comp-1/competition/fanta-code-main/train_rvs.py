1dd69358d4bd92669a102db1d14562ec47844342
rongxiaoqu <rongxiaoqu@gmail.com>
2023-04-21 05:36:53 +0800

    Update train_rvs.py

--- SMARTS-comp-1/competition/fanta-code-main/train_rvs.py
+++ SMARTS-comp-1/competition/fanta-code-main/train_rvs.py
@@ -149,7 +149,7 @@ def train(input_path, output_suffix, conservative_weight):
 
     # filename = scenario_path / (f"Agent-history-vehicle-{1}.pkl")
 
-    filename = "offline-datasets-master/us101_candid_list.pkl"
+    filename = "offline-datasets-master/offline_dataset/sumo_1/Agent-history-vehicle-1.pkl"
     # print(scenario_path)
     with open(filename, 'rb') as f:
         print(f)
